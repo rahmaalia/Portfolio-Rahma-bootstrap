06c314862b177daac6147f0202bc49964c4278ef
rahmaalia <rahmaaliaputri27@gmail.com>
2021-12-22 23:33:09 +0700

    skill

--- js/main.js
+++ js/main.js
@@ -5,3 +5,11 @@ $(selector).on('click', function () {
   $(selector).removeClass('active');
   $(this).addClass('active');
 });
+
+//javascript for navigation bar effect on scroll
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $('nav');
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
